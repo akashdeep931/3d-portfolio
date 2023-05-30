358da50c1232795347bdd677c1941cd9b30f98c3
akashdeep931 <akashdeepsingh931@gmail.com>
2023-05-30 15:20:57 +0100

    implement semantic HTML

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import Header from "./components/Header";
 
 const App = () => {
   return (
-    <body className="relative z-0 bg-primary">
+    <main className="relative z-0 bg-primary">
       <section className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Header />
       </section>
-    </body>
+    </main>
   );
 };
 
